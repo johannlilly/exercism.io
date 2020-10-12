3f262166c5075231fff396565483708ac60bc375
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-11 22:43:05 -0400

    julia/leap end
    I'm not sure if my solution is too verbose. I don't think I'm declaring default values for the Year enum properly, otherwise I wouldn't need to use a condition block to return true/false.

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -4,6 +4,23 @@
 Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
-function is_leap_year(year)
+
+@enum Year normal=false leap=true
+
+function is_leap_year(year)::Bool
+
+    isdivisibleby4   = year % 4   == 0
+    isdivisibleby100 = year % 100 == 0
+    isdivisibleby400 = year % 400 == 0
+
+    if isdivisibleby4 && isdivisibleby100 && isdivisibleby400
+        typeofyear = leap::Year
+    elseif isdivisibleby4 && !isdivisibleby100
+        typeofyear = leap::Year
+    else
+        typeofyear = normal::Year
+    end
+
+    typeofyear == leap::Year ? true : false
 
 end
