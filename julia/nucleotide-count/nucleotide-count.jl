b00cffd968ed833aca66851c626814bef556771a
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-17 20:32:31 -0400

    julia/nucleotide-count end

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -6,6 +6,19 @@ The frequency of each nucleotide within `strand` as a dictionary.
 Invalid strands raise a `DomainError`.
 
 """
-function count_nucleotides(strand)
+const valid_nucleotides = ['A', 'C', 'G', 'T']
 
+function count_nucleotides(strand::String)
+
+    nucleotides = Dict{Char,Int32}('A'=>0, 'C'=>0, 'G'=>0, 'T'=>0)
+    
+    for i in strand
+        if i in valid_nucleotides
+            nucleotides[i] = nucleotides[i] + 1
+        else
+            throw(DomainError(i, "strand with invalid nucleotides"))
+        end
+    end
+
+    return nucleotides
 end
