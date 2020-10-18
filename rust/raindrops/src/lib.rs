80b52d9e2cb97a8fe4b04d5ae9e3ce33be9410d4
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-18 11:28:01 -0400

    rust/raindrops end

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,17 @@
+/**
+ * Determine raindrop's sound
+ * @param {u32} n - single raindrop
+ * @returns {String} - the sound
+ */
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
-}
+    // unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut sound = String::from("");
+  
+    if n % 3 == 0 { sound += "Pling"; }
+    if n % 5 == 0 { sound += "Plang"; }
+    if n % 7 == 0 { sound += "Plong"; }
+    
+    if n%3!=0 && n%5!=0 && n%7!=0 { sound = n.to_string(); }
+  
+    return sound;
+  }
