2be54d39ff22eff2bbd4048096e43a4235ea0c31
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-18 09:37:18 -0400

    rust/leap end
    Because `return` will exit the function, I skipped using `else` and saved space

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,18 @@
+/**
+ * Determine if a year is a leap year
+ * @param {u64} year - the year to check
+ * @returns {bool}
+ */
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
-}
+    // unimplemented!("true if {} is a leap year", year)
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+  }
